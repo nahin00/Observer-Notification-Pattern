6932aca9781f3b2f9bc5be6c85f98e4e24d91133
nahin00 <contact.nahin@gmail.com>
2019-09-19 16:54:42 +0200

    Initial Commit

---
+++ Observer & Notification Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Observer & Notification Pattern
+//
+//  Created by Nahin Ahmed on 19.09.19.
+//  Copyright © 2019 NAhmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
